5a3855c748a76140c600c43aaac6bb55a6fa7c25
squidink7 <squidinkrf@gmail.com>
2023-06-15 10:21:28 +0930

    demo project

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace RiskiInsurance;
 
@@ -8,4 +10,24 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
+	void SayHello(object s, RoutedEventArgs e)
+	{
+		var newText = "";
+
+		if (String.IsNullOrEmpty(NameBox.Text))
+		{
+			newText = "Please type your name";
+		}
+		else if (NameBox.Text.ToLower() == "your name")
+		{
+			newText = "Okay smartass";
+		}
+		else
+		{
+			newText = $"Hello, {NameBox.Text}!";
+		}
+
+		ResultLabel.Text = newText;
+	}
 }
