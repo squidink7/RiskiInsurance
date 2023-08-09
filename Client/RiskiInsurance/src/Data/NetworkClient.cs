8a6c19cb8a60340dde047f1b3a9d6b453e9d45d0
Barry-B-Benson6 <josh2chapman@gmail.com>
2023-08-10 09:22:18 +0930

    Fixed one liner making code unreadable

--- Client/RiskiInsurance/src/Data/NetworkClient.cs
+++ Client/RiskiInsurance/src/Data/NetworkClient.cs
@@ -166,7 +166,9 @@ public static class NetworkClient
 		}
 		catch
 		{
-			return new HttpResponseMessage() { StatusCode = System.Net.HttpStatusCode.RequestTimeout };
+			return new HttpResponseMessage() { 
+				StatusCode = System.Net.HttpStatusCode.RequestTimeout
+			};
 		}
 	}
 } 
